c7fca6ea2606ed108423879f62638af91938863d
Ian Pfeffer <ian.pfeff@gmail.com>
2015-09-22 23:18:28 -0600

    saftey functions complete

--- start.js
+++ start.js
@@ -4,14 +4,16 @@
 var _ = require('lodash');
 var args = require('minimist')(process.argv.slice(2));
 var config = require('./config/all');
+var utils = require('./lib/utils');
 
 //check arguments
 if ((args.h) || (args.help)) {
     // they just want help
-    console.log(config.name);
+    console.log(config.help);
     process.exit(0);
 }
 
-if (args._.length !== 2) {
-    console.log('Omg Suck it');
+//The first arg should be the file you are manipulating
+if (args._[0]) {
+    // do it
 }
