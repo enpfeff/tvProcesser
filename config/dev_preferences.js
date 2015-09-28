736d1505f389ac59e82a9c3b679489f0e0d64263
Ian Pfeffer <ian.pfeff@gmail.com>
2015-09-27 19:15:24 -0600

    Seasonsnode entries/start-processTv.js out/stage/Veep.S01.720p.HDTV.x264-BTN.\[BTN\] -d

--- config/dev_preferences.js
+++ config/dev_preferences.js
@@ -24,6 +24,7 @@ module.exports = {
     // %o - original filename
     // %n - series name
     directoryStructure: '/%n/Season %s/%o',
+    directoryStructureSeason: '/%n/Season %s',
 
     plex : {
         //plex url
